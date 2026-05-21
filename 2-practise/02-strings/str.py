e53e052fc13f1be4e82b60821964da34792d79ce
Rejwan <rejwanmd246@gmail.com>
2026-05-21 23:55:45 +0600

    practice string in python

---
+++ 2-practise/02-strings/str.py
@@ -0,0 +1,51 @@
+str = "rejo"
+print(SystemError)
+
+#multiline string
+
+a = """this is
+multiline strings....
+ok!"""
+
+print(a)
+
+
+# string as array
+
+b = "Hello, World"
+print(b[1])
+
+
+# looping through a string
+
+for x in "apple":
+    print(x)
+
+
+# string length
+
+c = "Hello, World"
+print(len(c))
+
+
+# check string
+
+txt = "This is a string"
+print("is" in txt)  # true
+
+# use if
+
+txt = "This is a String"
+if "Str" in txt:
+    print("Yes, 'Str' is present in this string")
+
+# example of not
+txt = "I love flower"
+if "Love" not in txt:
+    print("No, 'love' is not present")
+
+
+# if NOT
+
+txt = "A string"
+print("this" not in txt)  # true
